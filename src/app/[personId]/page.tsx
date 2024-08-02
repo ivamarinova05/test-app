65755c75f6d45f7895558913b04a977d1287ae2e
Iva Marinova <iva.marinova@accedia.com>
2024-08-02 10:53:41 +0300

    make Person page server-side rendered

--- src/app/[personId]/page.tsx
+++ src/app/[personId]/page.tsx
@@ -1,10 +1,10 @@
-'use client'
-import {useGetAllUsers} from "../../hooks/useGetAllUsers"
+import axios from "axios";
 import {PersonCard} from "../components/PersonCard/PersonCard"
+import { tableRow } from "@/src/types/types";
 
-export default function Person ({params}: {params: {personId: string}}) {
-    const users = useGetAllUsers();
-    const user = users.find((currUser) => currUser.id === params.personId);
+export default async function Person ({params}: {params: {personId: string}}) {
+    const { data }  = await axios.get('http://localhost:3000/api');
+    const user = data.tableData.find((currUser: tableRow) => currUser.id === params.personId);
 
     return (
          <PersonCard person={user}/>
