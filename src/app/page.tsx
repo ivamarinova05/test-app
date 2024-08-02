66ba21e16ed60c3412b951cf2b69054bcc74514c
Iva Marinova <iva.marinova@accedia.com>
2024-08-02 10:46:11 +0300

    make the Home page a server component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 
-'use client'
-import {useGetAllUsers} from "../hooks/useGetAllUsers"
+
+import axios from "axios";
 import { UsersTable } from "./components/UsersTable/UsersTable";
 
-export default function Home() {
-  const tableData = useGetAllUsers();
+export default async function Home() {
+  const { data }  = await axios.get('http://localhost:3000/api');
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <UsersTable tableData={tableData} />
+      <UsersTable tableData={data.tableData} />
     </main>
   );
 }
